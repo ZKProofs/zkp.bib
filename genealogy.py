62891f3110384a816df684435604350ee5fca926
Jack Grigg <thestr4d@gmail.com>
2018-12-04 01:11:21 +0000

    Add implementation details via an 'implements' field
    This is a comma-separated list of entry keys, like 'buildson'.

--- genealogy.py
+++ genealogy.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import argparse
 import base64
 import bibtexparser
 import json
 
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--implements', action='store_true',
+                    help='show implementation links')
+args = parser.parse_args()
 
 # Parse BibTeX to get metadata
 with open('zkp.bib') as f:
@@ -13,9 +19,16 @@ buildson = [
     for entry in db.entries if 'buildson' in entry.keys()
 ]
 
+implements = [
+    (entry['ID'], entry['implements'])
+    for entry in db.entries if 'implements' in entry.keys()
+] if args.implements else []
+
 # Convert to Mermaid
 mermaid = 'graph TD\n' + '\n'.join([
     '%s --> %s' % (parent.strip(), entry[0]) for entry in buildson for parent in entry[1].split(',')
+] + [
+    '%s -.-> %s' % (parent.strip(), entry[0]) for entry in implements for parent in entry[1].split(',')
 ])
 
 # Construct Mermaid online viewer string
